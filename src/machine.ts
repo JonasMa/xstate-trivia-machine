e6f0930a53c2d63482ff2cdac4b7de85a1fbef86
r3dDoX <r3dDoX@users.noreply.github.com>
2022-03-16 07:16:42 +0100

    add basic machine from vis with actions and guards

--- src/machine.ts
+++ src/machine.ts
@@ -1,48 +1,79 @@
 import { assign, createMachine } from 'xstate'
 import { questions } from "./utils/quizData";
-import { options } from "./options";
+
+const INITIAL_CONTEXT = {
+  currentQuestion: 0,
+  currentQuestionDisplay: 1,
+  totalCorrectAnswers: 0,
+  questions,
+};
 
 export const machine = createMachine(
   {
-    id: 'Machine',
-    initial: 'welcome',
-    context: {
-      currentQuestion: 0,
-      currentQuestionDisplay: 1,
-      totalCorrectAnswers: 0,
-      questions
-    },
+    id: "QuizMachine",
+    initial: "welcome",
+    context: INITIAL_CONTEXT,
     states: {
       welcome: {
         on: {
-          START_QUIZ: 'quiz',
-        },
+          START_QUIZ: {
+            target: "#QuizMachine.quiz"
+          }
+        }
       },
       quiz: {
         always: {
-          target: 'results',
-          cond: 'allQuestionsAnswered',
+          cond: "allQuestionsAnswered",
+          target: "#QuizMachine.results"
         },
         on: {
           ANSWER_FALSE: {
-            actions: 'updateAnswer',
+            actions: "updateAnswer",
+            target: "#QuizMachine.quiz"
           },
           ANSWER_TRUE: {
-            actions: 'updateAnswer',
-          },
-        },
+            actions: "updateAnswer",
+            target: "#QuizMachine.quiz"
+          }
+        }
       },
       results: {
+        exit: "resetQuiz",
         on: {
-          PLAY_AGAIN: 'welcome',
-        },
-        exit: assign({
-          currentQuestion: 0,
-          currentQuestionDisplay: 1,
-          totalCorrectAnswers: 0,
-        }),
-      },
-    },
+          PLAY_AGAIN: {
+            target: "#QuizMachine.welcome"
+          }
+        }
+      }
+    }
   },
-  options
+  {
+    actions: {
+      updateAnswer: assign((ctx, {type}) => {
+        const answer = type === 'ANSWER_TRUE';
+        return {
+          questions: [
+            ...ctx.questions!.slice(0, ctx.currentQuestion),
+            {
+              ...ctx.questions![ctx.currentQuestion],
+              userAnswer: answer,
+              correct:
+                ctx.questions![ctx.currentQuestion].correctAnswer === answer,
+            },
+            ...ctx.questions!.slice(ctx.currentQuestion + 1),
+          ],
+          totalCorrectAnswers:
+            ctx.questions![ctx.currentQuestion].correctAnswer === answer
+              ? (ctx.totalCorrectAnswers += 1)
+              : ctx.totalCorrectAnswers,
+          currentQuestion: ctx.currentQuestion += 1,
+          currentQuestionDisplay: ctx.currentQuestionDisplay += 1,
+        };
+      }),
+      resetQuiz: assign(INITIAL_CONTEXT),
+    },
+    guards: {
+      allQuestionsAnswered: (ctx) => ctx.questions.every(question => question.correct !== undefined),
+    }
+  }
 )
